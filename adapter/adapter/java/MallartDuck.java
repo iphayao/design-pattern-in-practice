2701b2a20e2fe999e8f15d74d90c9a4a1c2cf1fe
Phayao Boonon <phayaob@gmail.com>
2017-07-21 21:32:44 +0700

    Add Adapter Pattern

---
+++ adapter/adapter/java/MallartDuck.java
@@ -0,0 +1,9 @@
+public class MallartDuck implements Duck {
+    public void quack() {
+        System.out.println("Quak");
+    }
+
+    public void fly() {
+        System.out.println("I'm fly");
+    }
+}
---
+++ adapter/adapter/java/WildTurkey.java
@@ -0,0 +1,11 @@
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
+
+public class WildTurkey implements Turkey {
+    public void gobble() {
+        System.out.println("Gobble gobble");
+    }
+
+    public void fly() {
+        System.out.println("I'm flying a short distance");
+    }
+}
