5db728d8bcb0bf1b832338829d22f46dc85b7a42
Phayao Boonon <phayaob@gmail.com>
2017-07-24 23:52:25 +0700

    Add Template Method Pattern

---
+++ template/java/BeverageTestDriveWithHook.java
@@ -0,0 +1,13 @@
+public class BeverageTestDriveWithHook {
+    public static void main(String[] args) {
+        TeaWithHook teaWithHook = new TeaWithHook();
+        CoffeeWithHook coffeeWithHook = new CoffeeWithHook();
+
+        System.out.println("\nMaking tea...");
+        teaWithHook.prepareRecipe();
+
+        System.out.println("\nMaking coffee...");
+        coffeeWithHook.prepareRecipe();
+
+    }
+}
