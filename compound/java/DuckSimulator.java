128b2248d06c84b800f2fbff72a92358c748278d
Phayao Boonon <phayaob@gmail.com>
2017-07-31 22:13:32 +0700

    Add decorator

--- compound/java/DuckSimulator.java
+++ compound/java/DuckSimulator.java
@@ -5,19 +5,21 @@ public class DuckSimulator {
     }
 
     void simulate() {
-        Quackable mallardDurk = new MallardDuck();
-        Quackable redheadDuck = new RedheadDuck();
-        Quackable duckCall = new DuckCall();
-        Quackable rubberQuack = new RubberDuck();
+        Quackable mallardDurk = new QuackCounter(new MallardDuck());
+        Quackable redheadDuck = new QuackCounter(new RedheadDuck());
+        Quackable duckCall = new QuackCounter(new DuckCall());
+        Quackable rubberQuack = new QuackCounter(new RubberDuck());
         Quackable gooseDuck = new GooseAdapter(new Goose());
 
-        System.out.println("\nDuck Simulator");
+        System.out.println("\nDuck Simulator: With Decorator");
 
         simulate(mallardDurk);
         simulate(redheadDuck);
         simulate(duckCall);
         simulate(rubberQuack);
         simulate(gooseDuck);
+
+        System.out.println("The duck quacked " + QuackCounter.getQuacks() + " times");
     }
 
     void simulate(Quackable duck) {
